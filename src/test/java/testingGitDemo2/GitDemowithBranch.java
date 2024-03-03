790a3a16bd3f9416326c8dfc99e4ce1c3e5f8b57
VersatileVitthal <versastilevitthal@gmail.com>
2024-03-03 15:18:09 +0530

    03-03-2024 Git branches

---
+++ src/test/java/testingGitDemo2/GitDemowithBranch.java
@@ -0,0 +1,16 @@
+package testingGitDemo2;
+
+import org.testng.annotations.Test;
+
+public class GitDemowithBranch 
+{
+  @Test
+  public void f() 
+  {
+	  
+	 System.out.println("This is for git branch testing"); 
+	    }
+  
+}
+
+
