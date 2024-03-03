077fea56214d2f9e23a07fb256d2e23385d1ac60
VersatileVitthal <versastilevitthal@gmail.com>
2024-03-03 11:51:31 +0530

    first commit2

---
+++ src/test/java/testingGit/TestGitDemo.java
@@ -0,0 +1,17 @@
+package testingGit;
+
+import org.testng.annotations.Test;
+
+public class TestGitDemo {
+  @Test
+  public void test1() 
+  {
+  
+  System.out.println("This is test1");
+  
+  }
+  
+  
+}
+
+
